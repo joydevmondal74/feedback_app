7d6e0cfcd6ca6b62ee9402dc9655a7d2167b388d
joydevmondal74 <joydevmondal8874@gmail.com>
2026-05-09 13:49:58 +0530

    changes

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -40,7 +40,8 @@ if (
   !formData.name.trim() ||
   !formData.phone.trim() ||
   !formData.email.trim() ||
-  !formData.address.trim()
+  !formData.address.trim() ||
+  !formData.feedback.trim()
 ) {
   alert("Please fill all the fields");
   return;
