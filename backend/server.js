426a23b6612c92581c5134fbe48fe94b97d43a44
joydevmondal74 <joydevmondal8874@gmail.com>
2026-05-12 18:29:08 +0530

    API changes

--- backend/server.js
+++ backend/server.js
@@ -9,9 +9,7 @@ const app = express();
 /* Middleware */
 
 app.use(cors({
-  origin: [
-    "https://feedback-app-alpha-wine.vercel.app" //Vercel URL
-  ],
+  origin: [process.env.FRONTEND_URL],
   methods: ["GET", "POST"],
   credentials: true
 }));
